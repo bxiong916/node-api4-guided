0e5de53472232886d25ee47aab8e09ea2532023b
Bill Xiong <xiong.bill@yahoo.com>
2021-03-20 23:19:42 -0700

    added start script and dynamic tcp port

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
+
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT;
+console.log(process.env);
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+// CI / CD
+// DEV ==> TEST ==> STAGING ==> PRODUCTION
