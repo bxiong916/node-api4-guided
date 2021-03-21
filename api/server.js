b4345e2298decfde4e7e3a3f23cdc42d8f0c7767
Bill Xiong <xiong.bill@yahoo.com>
2021-03-20 23:54:56 -0700

    added motd

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const apiRouter = require('./api-router.js');
+const apiRouter = require("./api-router.js");
 
 const server = express();
 
 server.use(helmet());
 
-server.use('/api', apiRouter);
+server.use("/api", apiRouter);
+
+server.get("/motd", (req, res) => {
+  const motd = process.env.MOTD || "Hello World!!!";
+  res.status(200).json({ motd: motd });
+});
 
 module.exports = server;
